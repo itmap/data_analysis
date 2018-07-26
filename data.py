af41e0b62e97d32c7c44761cd706295cc0db3b7b
jiyi.song <jiyi.song@ele.me>
2018-07-27 00:47:41 +0800

    files

--- data.py
+++ data.py
@@ -5,13 +5,14 @@ from pymongo import MongoClient
 
 client = MongoClient('mongo',27017)
 db = client.data
-
-with open('/home/ubuntu/workspace/spiders/logs/juejin/info.log', 'r') as f:
-    line = f.readline()
-    while line:
-        data = json.loads(line)
-        index = data['index']
-        table = db[index]
-        doc_id = data['document_id']
-        table.update({'document_id':doc_id},data,{'upsert': True})
+files = ['juejin.log']
+for fl in files:
+    with open(fl, 'r') as f:
         line = f.readline()
+        while line:
+            data = json.loads(line)
+            index = data['index']
+            table = db[index]
+            doc_id = data['document_id']
+            table.update({'document_id':doc_id},data,{'upsert': True})
+            line = f.readline()
