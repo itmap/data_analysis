f32f080f2c4aaf98610e7c95635fb4ae0839aa93
jiyi.song <jiyi.song@ele.me>
2018-07-27 00:14:27 +0800

    add data analysis

---
+++ data.py
@@ -0,0 +1,17 @@
+import pymongo
+import json
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost',27017)
+db = client.data
+
+with open('/home/ubuntu/workspace/spiders/logs/juejin/info.log', 'r') as f:
+    line = f.readline()
+    while line:
+        data = json.loads(line)
+        index = data['index']
+        table = db[index]
+        doc_id = data['document_id']
+        table.update({'document_id':doc_id},data,{'upsert': True})
+        line = f.readline()
