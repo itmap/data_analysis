e62621683a868cebf26b78b8653c5221aa292a7d
jiyi.song <jiyi.song@ele.me>
2018-07-30 23:23:56 +0800

    mongo connection

--- settings.py
+++ settings.py
@@ -2,7 +2,9 @@ import os
 
 from pymongo import MongoClient
 
+host = os.environ.get('MONGO_INITDB_HOST', 'localhost')
+port = os.environ.get('MONGO_INITDB_PORT', 27017)
 user = os.environ.get('MONGO_INITDB_ROOT_USERNAME', 'root')
 password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', 'Song123654')
-client = MongoClient('mongo', 27017, username=user, password=password)
+client = MongoClient(host, port, username=user, password=password)
 db = client.data
