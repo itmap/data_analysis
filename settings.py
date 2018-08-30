bb5e19c369a9932e55960027eca35ab99e424b33
zhangrj <zhangrj@ethicall.cn>
2018-08-30 17:50:44 +0800

    update

--- settings.py
+++ settings.py
@@ -2,11 +2,11 @@ import os
 
 from pymongo import MongoClient
 
-host = os.environ.get('MONGO_INITDB_HOST', 'localhost')
+host = os.environ.get('MONGO_INITDB_HOST', 'mongo')
 port = os.environ.get('MONGO_INITDB_PORT', 27017)
 user = os.environ.get('MONGO_INITDB_ROOT_USERNAME', 'root')
 password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', 'Song123654')
 client = MongoClient(host, port, username=user, password=password)
 db = client.data
 
-RABBITMQ_URL = os.environ.get('RABBITMQ_URL', 'amqp://guest:guest@localhost:5672/%2F')
+RABBITMQ_URL = os.environ.get('RABBITMQ_URL', 'amqp://guest:guest@rabbit:5672/%2F')
