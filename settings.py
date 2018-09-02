3a0b6b2554f2473ab5ab186091d38a9256d642f1
zhangrj <zhangrj@ethicall.cn>
2018-09-02 17:12:56 +0800

    update

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 from pymongo import MongoClient
 
-host = os.environ.get('MONGO_INITDB_HOST', '111.231.195.175')
+host = os.environ.get('MONGO_INITDB_HOST', 'localhost')
 port = os.environ.get('MONGO_INITDB_PORT', 27017)
 user = os.environ.get('MONGO_INITDB_ROOT_USERNAME', 'root')
 password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', 'Song123654')
