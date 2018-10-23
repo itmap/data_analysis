2e30479d599f22a07060b70a781cc7390320b93b
songcser <songjiyi2008@163.com>
2018-10-24 00:33:03 +0800

    add jieba text rank

---
+++ text_rank.py
@@ -0,0 +1,32 @@
+import jieba
+import jieba.analyse
+
+
+from settings import db, collections
+
+def handler_jieba_text_rank():
+    collection = db['article-juejin']
+
+    jieba.analyse.set_stop_words('stopword.txt')
+
+    for data in collection.find():
+        body = data['body']
+        doc_id = data['document_id']
+        ranks = []
+        for x, w in jieba.analyse.textrank(s, topK=30,
+            withWeight=True, allowPOS=('ns', 'n')):
+            ranks.append({x: w})
+        data = {
+            '$set': {
+                'jieba_text_rank': ranks
+            }
+        }
+        collection.update_one({'document_id': doc_id}, data, upsert=True)
+
+
+@click.command()
+@click.option('--collection', '-c', type=click.Choice(collections), multiple=True)
+def textrank(collection):
+    h = collection if collection else collections
+    if 'jieba' in h:
+        handler_jieba_text_rank()
