402eac7d6fe665236b5bfe7c0cb8fd509d3e5161
songcser <songjiyi2008@163.com>
2018-10-24 00:34:44 +0800

    fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import click
 import logging
 
 #from word_segment import segmentation
+from text_rank import textrank
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,6 +12,7 @@ def clis():
 
 
 #clis.add_command(segmentation)
+clis.add_command(textrank)
 
 if __name__ == "__main__":
     clis()
